c99ef8670c0fd164a57bf6fd4a00491d2e80c12d
Thomas1023 <league@WorthyMini-6.attlocal.net>
2017-09-10 12:59:16 -0700

    day two of lvl 3
    redid array list done
    working on stacks pop/push
    just finished intro to stacks
    starting text undo redo

--- src/IntroToArrayLists/GuestBook.java
+++ src/IntroToArrayLists/GuestBook.java
@@ -1,6 +1,37 @@
 package IntroToArrayLists;
 
-public class GuestBook {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class GuestBook implements ActionListener{
+	JFrame J = new JFrame();
+	JPanel P = new JPanel();
+	JButton Book = new JButton("Book");
+	JButton Look = new JButton("look");
+	ArrayList<String> names = new ArrayList<String>();
+	public static void main(String[] args) {
+		GuestBook GuestBook = new GuestBook();
+		GuestBook.setup();
+	}
+	void setup() {
+
+		
+		J.setVisible(true);
+		J.add(P);
+		
+		P.add(Book);
+		P.add(Look);
+		J.pack();
+		
+		Book.addActionListener(this);
+		Look.addActionListener(this);
+	}
 	// Create a GUI with two buttons. One button reads "Add Name" and the other button reads "View Names". 
 	// When the add name button is clicked, display an input dialog that asks the user to enter a name. Add
 	// that name to an ArrayList. When the "View Names" button is clicked, display a message dialog that displays
@@ -9,4 +40,18 @@ public class GuestBook {
 	// Guest #2: Sandy Summers
 	// Guest #3: Greg Ganders
 	// Guest #4: Donny Doners
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		JButton ButtonPressed =  (JButton) e.getSource();
+		if(ButtonPressed == Book) {
+			String NewName = JOptionPane.showInputDialog("Write you name under if you would like to book");
+			names.add(NewName);
+		} if(ButtonPressed == Look) {
+			for(int i=0;i< names.size(); i++ ) {
+				System.out.println(names.get(i));
+			}
+		}
+		
+		
+	}
 }
--- src/IntroToArrayLists/IntroToArrayLists.java
+++ src/IntroToArrayLists/IntroToArrayLists.java
@@ -1,20 +1,47 @@
 package IntroToArrayLists;
 
+import java.util.ArrayList;
+
 public class IntroToArrayLists {
 	public static void main(String[] args) {
 		//1. Create an array list of Strings
 		//   Don't forget to import the ArrayList class
-		
+		ArrayList<String> words = new ArrayList<String>();
 		//2. Add five Strings to your list
-		
+		words.add("teaki");
+		words.add("ramen");
+		words.add("code");
+		words.add("keybord");
+		words.add("space");
+		System.out.println("for");
 		//3. Print all the Strings using a standard for-loop
-		
+		for (int i = 0; i < words.size(); i++) {
+			System.out.println(words.get(i));
+		}
+		System.out.println("each");
 		//4. Print all the Strings using a for-each loop
-		
+		for(String j : words){
+			System.out.println(j);
+			
+		}
+		System.out.println("even");
 		//5. Print only the even numbered elements in the list.
-		
+		for (int i = 0; i < words.size(); i++) {
+			if(i%2==0)
+			System.out.println(words.get(i));
+		}
+		System.out.println("reverce");
 		//6. Print all the Strings in reverse order.
-		
+		for (int i = words.size()-1; i > 0 ; i--) {
+			System.out.println(words.get(i));
+		}
+		System.out.println("with e");
 		//7. Print only the Strings that have the letter 'e' in them.
+		for (int i = 0; i < words.size(); i++) {
+			if(words.get(i).contains("e")) {
+				System.out.println(words.get(i));
+			}
+			
+		}
 	}
 }
